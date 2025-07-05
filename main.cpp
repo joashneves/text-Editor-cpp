4f1752980f5be3179e4670fdf25bd95e6cae8963
joashneves <joashneves@gmail.com>
2025-07-05 19:53:20 -0300

    faz tudo funcionar

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 #include <cstring>
 #include <string>
 #include <iostream>
+#include <FL/Fl_File_Chooser.H> 
 
 // Classe da janela do editor
 class EditorWindow : public Fl_Double_Window {
@@ -33,6 +34,8 @@ public:
 
 int changed = 0;
 Fl_Text_Buffer *textbuf = new Fl_Text_Buffer();
+char filename[256] = "";
+char title[256] = "";
 
 // Função de callback para mudança no texto
 void changed_cb(int, int nInserted, int nDeleted,int, const char*, void* v) {
@@ -43,15 +46,150 @@ void changed_cb(int, int nInserted, int nDeleted,int, const char*, void* v) {
   std::cout << nInserted << '\n';
 }
 
+void save_file(char *newfile){
+  if (textbuf->savefile(newfile)){
+    fl_alert("Erro ao salvar o arquivo \'%s\' :\n%s", newfile, strerror(errno));
+  }
+  else {
+    strcpy(filename, newfile);
+  }
+  changed = 0;
+  textbuf->call_modify_callbacks();
+}
+
+void saveas_cb(void){
+  char *newfile;
+  newfile = fl_file_chooser("Salva arquivo? ", "*", filename);
+  if (newfile != NULL) save_file(newfile);
+}
+
+void save_cb(void) {
+  if(filename[0] == '\0'){
+    saveas_cb();
+    return;
+  }
+  else save_file(filename);
+}
+
+int check_save(void){
+  if(!changed) return 1;
+  int r = fl_choice("O arquivo não foi salvo \n Voce gostaria de salva-lo?", "Cancelar", "Salvar", "Descartar");
+  if (r == 1){
+    save_cb();
+    return !changed;
+  }
+  return (r == 2) ? 1 : 0;
+}
+
 // Callbacks de menu vazios por enquanto
-void new_cb(Fl_Widget*, void*) {}
-void open_cb(Fl_Widget*, void*) {}
+void new_cb(Fl_Widget*, void*) {
+  if(!check_save()) return;
+  filename[0] = '\0';
+  textbuf->select(0, textbuf->length());
+  textbuf->remove_selection();
+  changed = 0;
+  textbuf->call_modify_callbacks();
+}
+
+int loading = 0;
+void load_file(char *newfile, int ipos){
+  loading =1;
+  int insert = (ipos != -1);
+  changed = insert;
+  if(!insert) strcpy(filename, "");
+  int r;
+  if(!insert) r = textbuf->loadfile(newfile);
+  if(r){
+    fl_alert("Erro ao carregar o arquivo \'%s\' : \n%s.", newfile, strerror(errno));
+  }else{
+    if(!insert) strcpy(filename, newfile);
+  }
+  loading = 0;
+  textbuf->call_modify_callbacks();
+}
+
+
+void open_cb(Fl_Widget*, void*) {
+  if(!check_save()) return;
+
+  char *newfile = fl_file_chooser("Abrir arquivo?", "*", filename);
+  if(newfile != NULL) load_file(newfile, -1);
+}
+
+void replace_cb(Fl_Widget*, void* v){
+  EditorWindow* e= (EditorWindow*)v;
+  e->replace_dlg->show();
+}
+void replace2_cb(Fl_Widget*, void* v){
+  EditorWindow* e = (EditorWindow*)v;
+  const char *find = e->replace_find->value();
+  const char *replace = e->replace_with->value();
+
+  if(find[0] == '\0'){
+    e->replace_dlg->show();
+    return;
+  }
+  e->replace_dlg->hide();
+  int pos = e->editor->insert_position();
+  int found = textbuf->search_forward(pos, find, &pos);
+
+  if(found){
+    textbuf->select(pos, pos+strlen(find));
+    textbuf->remove_selection();
+    textbuf->insert(pos, replace);
+    textbuf->select(pos, pos+strlen(replace));
+    e->editor->insert_position(pos+strlen(replace));
+    e->editor->show_insert_position();
+  }
+  else fl_alert("No occurrences of \'%s\' found!", find);
+}
+
+void replall_cd(Fl_Widget*, void* v){
+  EditorWindow* e = (EditorWindow*)v;
+  const char *find = e->replace_find->value();
+  const char *replace = e->replace_with->value();
+
+  find = e->replace_find->value();
+  if(find[0] == '\0'){
+    e->replace_dlg->show();
+    return;
+  }
+  e->replace_dlg->hide();
+  e->editor->insert_position(0);
+  int times = 0;
+  for (int found = 1; found;){
+    int pos = e->editor->insert_position();
+    found = textbuf->search_forward(pos, find, &pos);
+    if(found){
+      textbuf->select(pos, pos+strlen(find));
+      textbuf->remove_selection();
+      e->editor->insert_position(pos+strlen(replace));
+      e->editor->show_insert_position();
+      times++;
+    }
+  }
+  if(time){
+    fl_message("Replaced %d ocurrences.", times);
+  }else{
+    fl_alert("NO OCCURRENCES OF \'%s\' found!", find);
+  }
+}
+
+void replcan_cb(Fl_Widget*, void* v){
+  EditorWindow* e = (EditorWindow*)v;
+  e->replace_dlg->hide();
+}
+
 void insert_cb(Fl_Widget*, void*) {}
-void save_cb(Fl_Widget*, void*) {}
-void saveas_cb(Fl_Widget*, void*) {}
+
 void view_cb(Fl_Widget*, void*) {}
 void close_cb(Fl_Widget*, void*) {}
-void quit_cb(Fl_Widget*, void*) { exit(0); }
+void quit_cb(Fl_Widget*, void*) { 
+  if(changed && check_save()) return;
+
+  exit(0);
+ }
+
 void undo_cb(Fl_Widget*, void*) {}
 
 void cut_cb(Fl_Widget*, void* v) {
@@ -63,7 +201,10 @@ void copy_cb(Fl_Widget*, void* v) {
   EditorWindow* e = (EditorWindow*)v;
   Fl_Text_Editor::kf_copy(0, e->editor);
 }
-void paste_cb(Fl_Widget*, void*) {}
+void paste_cb(Fl_Widget*, void* v) {
+  EditorWindow* e = (EditorWindow*)v;
+  Fl_Text_Editor::kf_paste(0, e->editor);
+}
 
 void delete_cb(Fl_Widget*, void* v) {
   textbuf->remove_selection();
@@ -99,7 +240,6 @@ strcpy(e->search, val);
 find2_cb(w, v);
 }
 
-
 int pos = e->editor->insert_position();
 int found = textbuf->search_forward(pos, e->search, &pos);
 if (found) {
@@ -110,8 +250,54 @@ e->editor->show_insert_position();
 }
 else fl_alert("No occurrences of \'%s\' found!", e->search);
 }
-void replace_cb(Fl_Widget*, void*) {}
-void replace2_cb(Fl_Widget*, void*) {}
+void set_title(Fl_Window* w){
+  if(filename[0] == '\0'){
+    strcpy(title, "Untitled");
+  }
+  else{
+    char *slash;
+    slash = strrchr(filename, '/');
+    #ifdef WIN32
+    if(slash == NULL)slash = strrchr(filename, '\\');
+    #endif
+    if(slash != NULL) strcpy(title, slash + 1);
+    else strcpy(title, filename); 
+  }
+  if (changed) strcat(title, " (Modificado)");
+  w->label(title);
+}
+
+
+Fl_Menu_Item menuitems[] = {
+  { "&File",              0, 0, 0, FL_SUBMENU },
+    { "&New File",        0, (Fl_Callback *)new_cb },
+    { "&Open File...",    FL_COMMAND + 'o', (Fl_Callback *)open_cb },
+    { "&Insert File...",  FL_COMMAND + 'i', (Fl_Callback *)insert_cb, 0, FL_MENU_DIVIDER },
+    { "&Save File",       FL_COMMAND + 's', (Fl_Callback *)save_cb },
+    { "Save File &As...", FL_COMMAND + FL_SHIFT + 's', (Fl_Callback *)saveas_cb, 0, FL_MENU_DIVIDER },
+    { "New &View", FL_ALT + 'v', (Fl_Callback *)view_cb, 0 },
+    { "&Close View", FL_COMMAND + 'w', (Fl_Callback *)close_cb, 0, FL_MENU_DIVIDER },
+    { "E&xit", FL_COMMAND + 'q', (Fl_Callback *)quit_cb, 0 },
+    { 0 },
+
+  { "&Edit", 0, 0, 0, FL_SUBMENU },
+    { "&Undo",       FL_COMMAND + 'z', (Fl_Callback *)undo_cb, 0, FL_MENU_DIVIDER },
+    { "Cu&t",        FL_COMMAND + 'x', (Fl_Callback *)cut_cb },
+    { "&Copy",       FL_COMMAND + 'c', (Fl_Callback *)copy_cb },
+    { "&Paste",      FL_COMMAND + 'v', (Fl_Callback *)paste_cb },
+    { "&Delete",     0, (Fl_Callback *)delete_cb },
+    { 0 },
+
+  { "&Search", 0, 0, 0, FL_SUBMENU },
+    { "&Find...",       FL_COMMAND + 'f', (Fl_Callback *)find_cb },
+    { "F&ind Again",    FL_COMMAND + 'g', find2_cb },
+    { "&Replace...",    FL_COMMAND + 'r', replace_cb },
+    { "Re&place Again", FL_COMMAND + 't', replace2_cb },
+    { 0 },
+
+  { 0 }
+};
+
 
 EditorWindow::EditorWindow(int W, int H, const char* title) : Fl_Double_Window(W, H, title) {
     editor = new Fl_Text_Editor(0, 30, W, H - 30);
@@ -119,6 +305,10 @@ EditorWindow::EditorWindow(int W, int H, const char* title) : Fl_Double_Window(W
     editor->textfont(FL_COURIER);
     editor->textcolor(FL_BLUE);
 
+      Fl_Menu_Bar *m = new Fl_Menu_Bar(0,0,640,30);
+  m->copy(menuitems);
+
+  
     replace_dlg = new Fl_Window(300, 105, "Replace");
     replace_find = new Fl_Input(70, 10, 200, 25, "Find:");
     replace_with = new Fl_Input(70, 40, 200, 25, "Replace:");
@@ -134,46 +324,16 @@ EditorWindow::~EditorWindow() {
     delete textbuf;
 }
 
-int main() {
-    EditorWindow *window = new EditorWindow(680, 440, "Editor de texto");
+int main(int argc, char **argv) {
 
-    // Menu
-    Fl_Menu_Item menuitems[] = {
-        { "&File", 0, 0, 0, FL_SUBMENU },
-            { "&New File", 0, new_cb },
-            { "&Open File...", FL_CTRL + 'o', open_cb },
-            { "&Insert File...", FL_COMMAND + 'i', insert_cb, 0, FL_MENU_DIVIDER },
-            { "&Save File", FL_COMMAND + 's', save_cb },
-            { "Save File &As...", FL_COMMAND + FL_SHIFT + 's', saveas_cb, 0, FL_MENU_DIVIDER },
-            { "New &View", FL_ALT + 'v', view_cb },
-            { "&Close View", FL_COMMAND + 'w', close_cb, 0, FL_MENU_DIVIDER },
-            { "E&xit", FL_COMMAND + 'q', quit_cb },
-            { 0 },
+  textbuf = new Fl_Text_Buffer;
 
-        { "&Edit", 0, 0, 0, FL_SUBMENU },
-            { "&Undo", FL_COMMAND + 'z', undo_cb, 0, FL_MENU_DIVIDER },
-            { "Cu&t", FL_COMMAND + 'x', cut_cb },
-            { "&Copy", FL_COMMAND + 'c', copy_cb },
-            { "&Paste", FL_COMMAND + 'v', paste_cb },
-            { "&Delete", 0, delete_cb },
-            { 0 },
+  EditorWindow* window = new EditorWindow(640, 400, "Text Editor");
+  
+  
+  window->show(1, argv);
 
-        { "&Search", 0, 0, 0, FL_SUBMENU },
-            { "&Find...", FL_COMMAND + 'f', find_cb },
-            { "F&ind Again", FL_COMMAND + 'g', find2_cb },
-            { "&Replace...", FL_COMMAND + 'r', replace_cb },
-            { "Re&place Again", FL_COMMAND + 't', replace2_cb },
-            { 0 },
+  if (argc > 1) load_file(argv[1], -1);
 
-        { 0 }
-    };
-
-    Fl_Menu_Bar *menu = new Fl_Menu_Bar(0, 0, 680, 30);
-    menu->copy(menuitems);
-
-    textbuf->add_modify_callback(changed_cb, window);
-    textbuf->call_modify_callbacks();
-
-    window->show();
-    return Fl::run();
+  return Fl::run();
 }
